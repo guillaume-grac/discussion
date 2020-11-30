62f0707e7117583b9a4a2695da3dc5a7005191b8
Guillaume <gui.grc@yahoo.com>
2020-11-30 18:36:23 +0100

    Discussion HTML et CSS En cours

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Advance's Chat | Accueil</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="nav justify-content-center nav-head">
+                <li class="nav-item"><a class="nav-link" href="php/inscription.php"><i class="fas fa-caret-right"></i> Inscription</a></li>
+                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-caret-right"></i> Connexion</a></li>
+                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-caret-right"></i> Profil</a></li>
+                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-caret-right"></i> Discussion</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="main-content container-fluid">
+            <h1>Advance's Chat</h1>
+            <section>
+                <img id="press" src="images/press.gif" alt="press2play">
+                <p>Tu es un 90's ?<br>Ce chat est pour toi !</p>
+            </section>
+        </section>
+    </main>
+    <footer>
+        <section class="footer-copyright text-center py-3">
+            <p>© 2020 Copyright : Guillaume G.</p>
+        </section>
+    </footer>
+</body>
+</html>
---
+++ php/inscription.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Advance's Chat | Inscription</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/inscription.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="nav justify-content-center nav-head">
+                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-caret-right"></i> Inscription</a></li>
+                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-caret-right"></i> Connexion</a></li>
+                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-caret-right"></i> Profil</a></li>
+                <li class="nav-item"><a class="nav-link" href=""><i class="fas fa-caret-right"></i> Discussion</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+        <section class="footer-copyright text-center py-3">
+            <p>© 2020 Copyright : Guillaume G.</p>
+        </section>
+    </footer>
+</body>
+</html>
